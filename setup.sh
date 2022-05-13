46bd12a21d35db6361ce929b209726d6142fc7fe
Antoine LASSERRE <98165865+Heronal@users.noreply.github.com>
2022-05-13 17:56:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,19 +4,19 @@ echo "sudo upgrade"
 sudo apt -y upgrade
 
 echo "sudo apache2"
-sudo apt -get -y install apache2
+sudo apt -y install apache2
 echo "sudo mysql"
-sudo apt -get install mysql-server
+sudo apt install mysql-server
 echo "sudo php"
-sudo apt -get -y install php8.0
-#sudo apt -get -y install php8.1 libapache2-mod-php8.1 php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl php8.1-bcmath unzip -y
+sudo apt -y install php8.0
+#sudo apt -y install php8.1 libapache2-mod-php8.1 php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl php8.1-bcmath unzip -y
 
 echo "sudo phpmyadmin"
-sudo apt -get install phpmyadmin
+sudo apt install phpmyadmin
 sudo cp /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
 sudo a2enconf phpmyadmin.conf
 sudo service apache2 restart
-sudo apt -get install cockpit
+sudo apt install cockpit
 
 
 sudo chmod +x serverSetup.sh
