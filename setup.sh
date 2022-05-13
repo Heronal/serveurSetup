72f96f4bbab2de478686cf8278405004ff422019
Antoine LASSERRE <98165865+Heronal@users.noreply.github.com>
2022-05-13 17:29:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,15 @@ echo "sudo update"
 sudo apt update
 echo "sudo upgrade"
 sudo apt -y upgrade
-sudo chmod u+x script
-sudo chmod 744 script
 
+echo "sudo apache2"
 sudo apt -get install apache2
+echo "sudo mysql"
 sudo apt -get install mysql-server
+echo "sudo php"
 sudo apt install php8.1-fpm php8.1 libapache2-mod-php8.1 php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl php8.1-bcmath unzip -y
 
+echo "sudo phpmyadmin"
 sudo apt -get install phpmyadmin
 sudo cp /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
 sudo a2enconf phpmyadmin.conf
