fde97ba44bd92187b5bfc843afdba289e34bdeed
Antoine LASSERRE <98165865+Heronal@users.noreply.github.com>
2022-05-14 15:24:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ sudo bash < <(curl https://raw.githubusercontent.com/Heronal/serveurSetup/master
 echo "parefeu fini"
 #sudo ./serverSetup. run
 
-
-
 sudo dpkg --configure -a
 echo "sudo update"
 sudo apt update
 echo "sudo upgrade"
 sudo apt -y upgrade
 
+sudo service apache2 restart
+sudo apt -y install cockpit
 
 echo "sudo apache2"
 sudo apt -y install apache2
@@ -20,11 +20,12 @@ sudo apt -y install mysql-server
 echo "sudo php"
 sudo apt -y install php8.0
 
+
+echo "kill pid"
+sudo killall dpkg
 echo "sudo phpmyadmin"
 sudo apt -y install phpmyadmin
 
-sudo service apache2 restart
-sudo apt -y install cockpit
 
 
 
